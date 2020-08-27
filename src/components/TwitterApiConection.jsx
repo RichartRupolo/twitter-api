a759f43d50a67d2435294fd94215c943705ec184
dev2bzs <dev2@bzs.com.br>
2020-08-27 11:17:58 -0300

    adicionado o componente de api

---
+++ src/components/TwitterApiConection.jsx
@@ -0,0 +1,54 @@
+import React, { Component } from 'react'
+
+
+class MyComponent extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            error: null,
+            isLoaded: false,
+            items: []
+        };
+    }
+
+    componentDidMount() {
+        fetch("https://api.example.com/items")
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    this.setState({
+                        isLoaded: true,
+                        items: result.items
+                    });
+                },
+                // Nota: É importante lidar com os erros aqui
+                // em vez de um bloco catch() para não recebermos
+                // exceções de erros dos componentes.
+                (error) => {
+                    this.setState({
+                        isLoaded: true,
+                        error
+                    });
+                }
+            )
+    }
+
+    render() {
+        const { error, isLoaded, items } = this.state;
+        if (error) {
+            return <div>Error: {error.message}</div>;
+        } else if (!isLoaded) {
+            return <div>Loading...</div>;
+        } else {
+            return (
+                <ul>
+                    {items.map(item => (
+                        <li key={item.name}>
+                            {item.name} {item.price}
+                        </li>
+                    ))}
+                </ul>
+            );
+        }
+    }
+}
